ef35e0ed0cdd42e98ba6b8105a5bf9406c621bf3
Chidleev <chidleev22@gmail.com>
2023-09-16 20:40:16 +0300

    a

--- js/bgScript.js
+++ js/bgScript.js
@@ -1,9 +1,17 @@
-// Функция для обработки события прокрутки
-function handleScroll() {
-    // Рассчитываем значение размытия в зависимости от прокрутки
+window.addEventListener('scroll', (event) => {
     let blurValue = Math.min(10, (window.scrollY / 150));
-    // Применяем размытие к фоновому изображению
     document.querySelector('#bg_img').style.filter = `blur(${blurValue}px)`;
+});
+
+function toPersents(vec) {
+    return {
+        x: `${(1+vec.x) * 50}%`,
+        y: `${(1+vec.y) * 50}%`
+    }
 }
-// Добавляем обработчик события прокрутки страницы
-window.addEventListener('scroll', handleScroll);
+
+window.addEventListener('mousemove', (event) => {
+    const toMouseDir = { x: event.clientX / window.innerWidth, y: event.clientY / window.innerHeight }
+
+    document.querySelector('#bg_img').style.backgroundPosition = `${toPersents(toMouseDir).x} ${toPersents(toMouseDir).y}`
+})
